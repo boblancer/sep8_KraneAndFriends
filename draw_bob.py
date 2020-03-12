c8d1ab47542485e5addcfd11e50682aaadb4de78
Bob <janekarnkit2013@hotmail.com>
2020-03-12 17:43:50 +0700

    refactor

---
+++ draw_bob.py
@@ -0,0 +1,17 @@
+class Simple_drawing_window4(QWidget):
+    def __init__(self):
+        QWidget.__init__(self,None)
+        self.setWindowTitle("Simple Drawing 4")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+        
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,120))
+        p.drawPolygon([
+            QPoint(120,120) , QPoint(0,50) ,QPoint(50,50), QPoint(-50,-40)
+        ])
+        p.drawPixmap(QRect(200,100,320,320),self.rabbit)
+        p.end()
--- draw_widget.py
+++ draw_widget.py
@@ -2,6 +2,9 @@ import sys
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+from draw_bey import *
+from draw_toon import *
+from draw_bob import *
 
 class Simple_drawing_window(QWidget):
     def __init__(self):
@@ -23,23 +26,6 @@ class Simple_drawing_window(QWidget):
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
 
-class Simple_drawing_window4(QWidget):
-    def __init__(self):
-        QWidget.__init__(self,None)
-        self.setWindowTitle("Simple Drawing 4")
-        self.rabbit = QPixmap("images/rabbit.png")
-
-        
-    def paintEvent(self,e):
-        p = QPainter()
-        p.begin(self)
-        p.setPen(QColor(0,0,0))
-        p.setBrush(QColor(0,127,120))
-        p.drawPolygon([
-            QPoint(120,120) , QPoint(0,50) ,QPoint(50,50), QPoint(-50,-40)
-        ])
-        p.drawPixmap(QRect(200,100,320,320),self.rabbit)
-        p.end()
 
 def main():
     app = QApplication(sys.argv)
